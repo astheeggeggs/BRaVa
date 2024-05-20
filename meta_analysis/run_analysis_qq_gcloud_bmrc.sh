42e05d81833221225188b49c3e80864dbe9fa50a
astheeggeggs <duncan.stuart.palmer@gmail.com>
2024-05-20 14:48:22 +0100

    updated to include burden plotting

--- meta_analysis/run_analysis_qq_gcloud_bmrc.sh
+++ meta_analysis/run_analysis_qq_gcloud_bmrc.sh
@@ -5,7 +5,10 @@
 
 FILE_GENE=$1
 OUT=$2
+new_OUT=$(echo "$OUT" | sed "s/\.pdf/_burden.pdf/")
 
 echo "Rscript analysis_qq.r --analysis_results_file $FILE_GENE --include_gene_names --out $OUT"
 Rscript analysis_qq.r --analysis_results_file $FILE_GENE --include_gene_names --out $OUT
-Rscript analysis_qq.r --analysis_results_file $FILE_GENE --burden_only_plot --include_gene_names --out burden_only_$OUT
+
+echo "Rscript analysis_qq.r --analysis_results_file $FILE_GENE --include_gene_names --out $new_OUT"
+Rscript analysis_qq.r --analysis_results_file $FILE_GENE --burden_only_plot --include_gene_names --out $new_OUT
